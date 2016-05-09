3310a14a425e7fda1b8433bfcca6bcdb5a99783f
JustF <folkerthoogenraad@hotmail.nl>
2016-05-09 11:38:14 +0200

    Added a simple database class setup

---
+++ src/net/apryx/dnd/database/Database.java
@@ -0,0 +1,15 @@
+package net.apryx.dnd.database;
+
+import net.apryx.dnd.items.Item;
+import net.apryx.dnd.world.Location;
+
+//TODO refactor
+public class Database {
+	
+	protected static Item[] items;
+	protected static Location[] locations;
+	
+	static {init();} private static void init(){
+		
+	}
+}
