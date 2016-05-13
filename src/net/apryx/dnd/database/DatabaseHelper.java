5ae7efb5250c1521056717c6c9d6ef602172d26e
JustF <408992@student.saxion.nl>
2016-05-13 12:51:45 +0200

    Changed the database and drop some stuff

--- src/net/apryx/dnd/database/DatabaseHelper.java
+++ src/net/apryx/dnd/database/DatabaseHelper.java
@@ -2,6 +2,8 @@ package net.apryx.dnd.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseHelper {
 	
@@ -13,8 +15,48 @@ public class DatabaseHelper {
 		createConnection();
 	}
 	
+	public void createTables(){
+		if(!checkConnection()){
+			System.err.println("Error Creating Connection");
+			return;
+		}
+		
+		Statement statement = null;
+		try{
+			statement = connection.createStatement();
+
+			statement.execute("DROP TABLE IF EXISTS Item");
+			statement.execute("DROP TABLE IF EXISTS Weapon");
+			statement.execute("DROP TABLE IF EXISTS Armor");
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			if(statement != null)
+				try{statement.close();}catch(Exception e){e.printStackTrace();}
+		}
+		
+	}
+	
 	public String getDatabase() {
 		return database;
+	}
+	
+	private boolean checkConnection(){
+		if(connection == null){
+			createConnection();
+			return true;
+		}else{
+			try {
+				if(connection.isClosed()){
+					createConnection();
+				}
+				return true;
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return false;
+			}
+		}
 	}
 	
 	private void createConnection(){
